6a1e8c72109f7fae30b415e79297104d2568cb9d
Sylvie <sylvie.veyres@gmail.com>
2017-05-24 12:05:07 +0200

    add style and pannel bootstrap on blog

--- template-parts/content_loop_blog.php
+++ template-parts/content_loop_blog.php
@@ -4,12 +4,17 @@ foreach ($posts as $post) {
     $tags = wp_get_post_tags($post->ID);
     ?>
     <article>
-        <img src="http://lorempixel.com/110/150/" alt="img_product">
-        <div>
-            <h4 class="<?php echo $tags[0]->name ?>"><?php echo $tags[0]->name ?></h4>
-            <h5><a href=" <?php the_permalink(); ?> "><?php the_title(); ?></a></h5>
-            <span><?php echo get_post_meta( $post->ID,'price', true ) ?> €</span>
+
+        <div class="panel panel-default">
+            <div class="panel-heading"><a href=" <?php the_permalink(); ?> "><?php the_title(); ?></a></div>
+            <div class="panel-body">
+                <img src="http://lorempixel.com/200/150/" alt="img_product">
+                <?php the_content(); ?></h4>
+            </div>
+            <div class="panel-footer"><?php the_date(); ?></div>
         </div>
+
+
     </article>
 
 <?php };?>
